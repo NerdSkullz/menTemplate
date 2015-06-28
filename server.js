2b2e847a1e6b15bd8006087647030b5f4ea84945
Pietro Di Marzo <NerdSkullz@Pietros-MacBook-Pro.local>
2015-06-28 22:37:56 +1000

    first prototype of the MEN template app

---
+++ server.js
@@ -0,0 +1,52 @@
+
+/**
+ * Module dependencies
+ */
+
+var fs = require('fs'),
+    express = require('express'),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    config = require('config'),
+    sass = require('node-sass'),
+    middleware = require('node-sass-middleware');
+
+var app = module.exports = express();
+
+// adding the sass middleware
+app.use(
+   middleware({
+       src: __dirname + '/app/assets/stylesheet', 
+       dest: __dirname + '/public',
+       debug: true,       
+   })
+);
+
+var port = process.env.PORT || 3000;
+
+// Connect to mongodb
+var connect = function () {
+  var options = { server: { socketOptions: { keepAlive: 1 } } };
+  mongoose.connect(config.db, options);
+};
+connect();
+
+mongoose.connection.on('error', console.log);
+mongoose.connection.on('disconnected', connect);
+
+// Bootstrap models
+fs.readdirSync(__dirname + '/app/models').forEach(function (file) {
+  if (~file.indexOf('.js')) require(__dirname + '/app/models/' + file);
+});
+
+// Bootstrap passport config
+require('./config/passport')(passport, config);
+
+// Bootstrap application settings
+require('./config/express')(app, passport);
+
+// Bootstrap routes
+require('./config/routes')(app, passport);
+
+app.listen(port);
+console.log('Express app started on port ' + port);
