d69eeae2003e7b0a267a3fcab114607430010a94
Pietro Di Marzo <NerdSkullz@Pietros-MacBook-Pro.local>
2015-07-18 02:43:04 +1000

    gulp file now start mongodb, node app and watches for js changes to pack every script in just one file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    exec = require('child_process').exec;
+
+
+function runCommand(command) {
+  return function (cb) {
+    exec(command, function (err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    });
+  }
+}    
 
 gulp.task('scripts', function() {
   return gulp.src(['app/assets/javascript/*.js'])
@@ -7,6 +19,12 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('default', ['scripts'] , function () {
+// Start MongoDb
+
+gulp.task('start-mongo', runCommand('mongod --dbpath /data/'));
+gulp.task('stop-mongo', runCommand('mongo --eval "use admin; db.shutdownServer();"'));
+gulp.task('start-app', runCommand('npm start'));
+
+gulp.task('default', ['start-mongo', 'start-app', 'scripts'] , function () {
   gulp.watch('app/assets/javascript/*.js', ['scripts']);
 })
